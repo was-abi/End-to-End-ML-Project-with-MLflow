50184c71690ab498a0308392533dc72531982f99
Abhishek Gajul <abhi123gajul@gmail.com>
2023-08-10 16:51:40 -0400

    Requirements added

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 project_name = "mlProject"
-
+#create a whole folder structure
 list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",

700e0d4a479e198a5db64e382ea157117d5022cf
Abhishek Gajul <abhi123gajul@gmail.com>
2023-08-11 21:21:17 -0400

    Data validation stage added

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,6 +1,6 @@
 from src.mlProject.constants import *
 from src.mlProject.utils.common import read_yaml, create_directories
-from src.mlProject.entity.config_entity import DataIngestionConfig
+from src.mlProject.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
 #define on class configuration manager
 #5. Update the configuration manager in src config
 
@@ -30,3 +30,18 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -9,3 +9,10 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
 
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    unzip_data_dir: Path
+    all_schema: dict #i want to store all the schema as a dictionary file

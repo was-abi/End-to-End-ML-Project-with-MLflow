ecd07c92d703ad5606df0d42186a9f9f97b3be4a
Abhishek Gajul <abhi123gajul@gmail.com>
2023-08-18 15:53:29 -0400

    Updated some Frontend elements

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def index():
         return render_template('index.html')
 
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0",port=8080,debug=True)
-    app.run(host="0.0.0.0",port=8080)
+    app.run(host="0.0.0.0",port=8080,debug=True)
+    #app.run(host="0.0.0.0",port=8080)
